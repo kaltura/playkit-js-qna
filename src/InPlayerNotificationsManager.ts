8e2c52fdf0b0afe246c80b66c1bb2ef582e5cc24
ainvoner <shai.ainvoner@kaltura.com>
2019-08-28 13:04:05 +0300

    WEBC-1450 - adding notifications DVR / VDO support
    handling two separate notifications arrays (announcements /
    answersOnAir) and adding listener to the players’ ‘seeking’ event

--- src/InPlayerNotificationsManager.ts
+++ src/InPlayerNotificationsManager.ts
@@ -22,13 +22,17 @@ const logger = getContribLogger({
     module: "qna-plugin"
 });
 
+const ReasonableSeekThreshold: number = 7 * 1000;
+
 const AnnouncementAutoCloseDuration: number = 60 * 1000;
+
 //TODO [sa] add log messages
 export class InPlayerNotificationsManager {
     private _messageEventManager: EventManager = new EventManager();
     private _cuePointEngine: CuepointEngine<QnaMessage> | null = null;
     private _playerApi: PlayerAPI;
-    private _notifications: QnaMessage[] = [];
+    private _announcements: QnaMessage[] = [];
+    private _answersOnAir: QnaMessage[] = [];
     private _currentNotification: QnaMessage | null = null;
 
     public get messageEventManager(): EventManager {
@@ -48,7 +52,8 @@ export class InPlayerNotificationsManager {
     }
 
     public unregister() {
-        this._notifications = [];
+        this._announcements = [];
+        this._answersOnAir = [];
         this._cuePointEngine = new CuepointEngine<QnaMessage>([]);
         //todo unregister push notification
         //todo unregister from player event
@@ -71,19 +76,37 @@ export class InPlayerNotificationsManager {
                 }
             });
         if (wasUpdated) {
-            this._cuePointEngine = new CuepointEngine<QnaMessage>(this._notifications, 10 * 1000);
+            this._cuePointEngine = new CuepointEngine<QnaMessage>(
+                this._getNotificationsForHandling(),
+                ReasonableSeekThreshold
+            );
         }
+    }
+
+    private _addPlayerListeners() {
+        const { kalturaPlayer, eventManager } = this._playerApi;
+        eventManager.listen(kalturaPlayer, kalturaPlayer.Event.TIMED_METADATA, (event: any) => {
+            this._onTimedMetadataLoaded(event);
+        });
+        eventManager.listen(kalturaPlayer, kalturaPlayer.Event.SEEKING, (event: any) => {
+            this._cuePointEngine = new CuepointEngine<QnaMessage>(
+                this._getNotificationsForHandling(),
+                ReasonableSeekThreshold
+            );
+        });
     }
 
     private _handleQnaMessage(newQnaMessage: QnaMessage): boolean {
         let wasUpdated = false;
-        let notificationIndex = this._notifications.findIndex(qnaMessage => {
-            return qnaMessage.id === newQnaMessage.id;
-        });
+        let notificationIndex = this._getRelevantNotificationArray(newQnaMessage).findIndex(
+            qnaMessage => {
+                return qnaMessage.id === newQnaMessage.id;
+            }
+        );
 
         if (notificationIndex === -1) {
             this._updateQnaMessageEndTime(newQnaMessage);
-            this._notifications.push(newQnaMessage);
+            this._getRelevantNotificationArray(newQnaMessage).push(newQnaMessage);
             wasUpdated = true;
         }
         return wasUpdated;
@@ -94,13 +117,6 @@ export class InPlayerNotificationsManager {
             qnaMessage.tags.indexOf("Annotation_Deleted") > -1
                 ? qnaMessage.startTime + 1 //need to remove at once (adding one to ease the sorting by end/start time)
                 : qnaMessage.startTime + AnnouncementAutoCloseDuration; //one minute after start time (for hiding the announcement in player)
-    }
-
-    private _addPlayerListeners() {
-        const { kalturaPlayer, eventManager } = this._playerApi;
-        eventManager.listen(kalturaPlayer, kalturaPlayer.Event.TIMED_METADATA, (event: any) => {
-            this._onTimedMetadataLoaded(event);
-        });
     }
 
     private _onTimedMetadataLoaded(event: any): void {
@@ -131,11 +147,11 @@ export class InPlayerNotificationsManager {
 
     private _handleSnapshotData(lastShow: QnaMessage | null) {
         if (lastShow) {
-            this._showCurrentAnnouncement(lastShow);
+            this._showCurrentNotification(lastShow);
         } else {
             //if there is no announcement to show - make sure to clear current announcement if needed
             // (if user seeked while an announcement was displayed)
-            this._hideCurrentAnnouncement();
+            this._hideCurrentNotification();
         }
     }
 
@@ -144,7 +160,7 @@ export class InPlayerNotificationsManager {
         let lastToHide = this._getMostRecentObject(hideArray);
         // only show objects
         if (!lastToHide && lastToShow) {
-            this._showCurrentAnnouncement(lastToShow);
+            this._showCurrentNotification(lastToShow);
         }
         // only hide objects + displaying a notification in player (if we are not displaying there is nothing to be done)
         if (!lastToShow && lastToHide && this._currentNotification) {
@@ -155,7 +171,7 @@ export class InPlayerNotificationsManager {
                     : false;
             });
             if (currentToRemove) {
-                this._hideCurrentAnnouncement();
+                this._hideCurrentNotification();
             }
         }
         // if exist both show and hide objects
@@ -163,11 +179,11 @@ export class InPlayerNotificationsManager {
             // if most recent object is a show object
             if (lastToShow.startTime > lastToHide.startTime) {
                 // since this message comes from the delta object if must be at least as recent as our current displayed notification
-                this._showCurrentAnnouncement(lastToShow);
+                this._showCurrentNotification(lastToShow);
             } else {
                 if (lastToShow.id === lastToHide.id) {
                     //since current displayed message, if exists, must be older than the data returned from the delta object -  we can remove all
-                    this._hideCurrentAnnouncement();
+                    this._hideCurrentNotification();
                 } else {
                     let lastShowToRemove = hideArray.find(message => {
                         return lastToShow ? lastToShow.id === message.id : false;
@@ -175,9 +191,9 @@ export class InPlayerNotificationsManager {
                     lastShowToRemove
                         ? // if lastShow was found in hide array we need to hide everything because lastToShow is more recent than the current displayed notification
                           // so if need to hide it - our current notification should be removed as well.
-                          this._hideCurrentAnnouncement()
+                          this._hideCurrentNotification()
                         : // if lastToShow wasn't fount in hide array, there is no need to hide it yet - so will need to show it.
-                          this._showCurrentAnnouncement(lastToShow);
+                          this._showCurrentNotification(lastToShow);
                 }
             }
         }
@@ -190,7 +206,7 @@ export class InPlayerNotificationsManager {
         return sortedLastFirst && sortedLastFirst[0] ? sortedLastFirst[0] : null;
     }
 
-    private _showCurrentAnnouncement(newMessage: QnaMessage) {
+    private _showCurrentNotification(newMessage: QnaMessage) {
         if (!this._currentNotification || newMessage.id !== this._currentNotification.id) {
             this._currentNotification = newMessage;
             if (this._messageEventManager) {
@@ -199,10 +215,45 @@ export class InPlayerNotificationsManager {
         }
     }
 
-    private _hideCurrentAnnouncement() {
+    private _hideCurrentNotification() {
         this._currentNotification = null;
         if (this._messageEventManager) {
             this._messageEventManager.emit("hideAnnouncement");
         }
+    }
+
+    /**
+     * Announcement should only be visible during live streaming (not in VOD / DVR mode)
+     * AnswerOnAir should be visible always
+     * @return QnaMessage[] : AnswerOnAir only / All
+     */
+    private _getNotificationsForHandling() {
+        //if user is currently watching live streaming (not DVR / VOD)
+        if (this._isLive() && this._isOnLiveEdge()) {
+            return this._announcements.concat(this._answersOnAir);
+        }
+        //if user is in DVR mode / VOD
+        return this._answersOnAir;
+    }
+
+    private _getRelevantNotificationArray(qnaMessage: QnaMessage) {
+        return qnaMessage.type === QnaMessageType.Announcement
+            ? this._announcements
+            : this._answersOnAir;
+    }
+
+    /**
+     * returns a boolean to detect if player is on live edge with buffer of 1 second
+     * indication if user is watching DVR mode at the moment
+     * @returns {boolean} - is player on live edge
+     */
+    private _isOnLiveEdge(): boolean {
+        return (
+            this._playerApi.kalturaPlayer.currentTime >= this._playerApi.kalturaPlayer.duration - 1
+        );
+    }
+
+    private _isLive(): boolean {
+        return this._playerApi.kalturaPlayer.isLive();
     }
 }
