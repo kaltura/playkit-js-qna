15eb8b215ca684565ae126f462ddb191759480a4
ainvoner <shai.ainvoner@kaltura.com>
2019-09-24 13:52:45 +0300

    findIndex is not supported in IE11 - using own impl.

--- src/chatMessagesAdapter.ts
+++ src/chatMessagesAdapter.ts
@@ -279,8 +279,8 @@ export class ChatMessagesAdapter {
             if (message.willBeAnsweredOnAir) {
                 return message;
             }
-            let aoaReplyIndex = (message.replies || []).findIndex((reply: QnaMessage) => {
-                return reply.isAoAAutoReply;
+            let aoaReplyIndex = Utils.findIndexById(message.replies || [], item => {
+                return item.isAoAAutoReply;
             });
             if (aoaReplyIndex > -1) {
                 return { ...message, willBeAnsweredOnAir: true };
