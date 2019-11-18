3d753c12a5794737f0bee152d245bebd838282a8
ainvoner <2538825+ainvoner@users.noreply.github.com>
2019-11-18 10:22:58 +0200

    feat(UI): removing lock icon from input (#63) WEBC-1645

--- src/components/auto-expand-text-area/autoExpandTextArea.tsx
+++ src/components/auto-expand-text-area/autoExpandTextArea.tsx
@@ -3,233 +3,217 @@ import * as styles from "./autoExpandTextArea.scss";
 import classNames from "classnames";
 
 interface AutoExpandTextAreaProps {
-    placeholder?: string;
-    onSubmit: (text: string) => void;
-    enableBlackInputTheme?: boolean;
-    initialFocus?: boolean;
-    alwaysOpen?: boolean;
-    disabled?: boolean;
-    showLockIcon?: boolean;
-    enableAnimation?: boolean;
-    onFocusOut?: () => void;
-    onFocusIn?: () => void;
+  placeholder?: string;
+  onSubmit: (text: string) => void;
+  enableBlackInputTheme?: boolean;
+  initialFocus?: boolean;
+  alwaysOpen?: boolean;
+  disabled?: boolean;
+  enableAnimation?: boolean;
+  onFocusOut?: () => void;
+  onFocusIn?: () => void;
 }
 
 interface AutoExpandTextAreaState {
-    text: string;
-    open: boolean;
-    bleepingAnimation: boolean;
+  text: string;
+  open: boolean;
 }
 
 const MAX_NUM_OF_CHARS = 500;
 const MAX_HEIGHT = 103;
-const AnimationDuration = 700;
 
 export class AutoExpandTextArea extends Component<
-    AutoExpandTextAreaProps,
-    AutoExpandTextAreaState
-> {
-    private _textareaContainer: any = null;
-    private _textAreaRef: HTMLTextAreaElement | null = null;
-    private _actionsContainer: HTMLElement | null = null;
-    private _sendButtonRef: HTMLButtonElement | null = null;
-    private _allowClickTimeout: ReturnType<typeof setTimeout> | null = null;
+  AutoExpandTextAreaProps,
+  AutoExpandTextAreaState
+  > {
+  private _textareaContainer: any = null;
+  private _textAreaRef: HTMLTextAreaElement | null = null;
+  private _actionsContainer: HTMLElement | null = null;
+  private _sendButtonRef: HTMLButtonElement | null = null;
+  private _allowClickTimeout: ReturnType<typeof setTimeout> | null = null;
 
-    static defaultProps = {
-        placeholder: "",
-        enableBlackInputTheme: false,
-        disabled: false,
-        showLockIcon: true,
-        enableAnimation: false
-    };
+  static defaultProps = {
+    placeholder: "",
+    enableBlackInputTheme: false,
+    disabled: false,
+    enableAnimation: false
+  };
 
-    state: AutoExpandTextAreaState = { text: "", open: false, bleepingAnimation: false };
+  state: AutoExpandTextAreaState = { text: "", open: false };
 
-    componentDidMount(): void {
-        if (this.props.alwaysOpen) {
-            this.setState({ open: true });
-        }
-
-        if (!this._textareaContainer) {
-            return;
-        }
-
-        this._textareaContainer.addEventListener("focusin", this._handleFocusIn);
-        this._textareaContainer.addEventListener("focusout", this._handleFocusOut);
-
-        if (this.props.initialFocus) {
-            this.focus();
-        }
+  componentDidMount(): void {
+    if (this.props.alwaysOpen) {
+      this.setState({ open: true });
     }
 
-    private _handleFocusIn = () => {
-        if (this._allowClickTimeout) {
-            clearTimeout(this._allowClickTimeout);
-            this._allowClickTimeout = null;
-        }
-
-        this.setState({ open: true, bleepingAnimation: true });
-        setTimeout(() => {
-            this.setState({ bleepingAnimation: false });
-        }, AnimationDuration);
-
-      if (this.props.onFocusIn) {
-        this.props.onFocusIn();
-      }
-    };
-
-    private _handleFocusOut = (e: any) => {
-        if (this._isElementOfComponent(e.relatedTarget)) {
-            return;
-        }
-
-        if (!this.props.alwaysOpen) {
-            // this helps to catch the click on an outside element (like, button) when clicking outsides the element.
-            // otherwise the click is missed and swallowed.
-            this._allowClickTimeout = setTimeout(() => {
-                this.setState(() => ({ open: false, bleepingAnimation: false }));
-            }, 200);
-        }
-
-        if (this.props.onFocusOut) {
-            this.props.onFocusOut();
-        }
-    };
-
-    focus = () => {
-        if (this._textAreaRef) {
-            this._textAreaRef.focus();
-        }
-    };
-
-    private _handleOnInputChange = (event: any) => {
-        this.setState({ text: event.target.value });
-        this._resize();
-    };
-
-    private _resize = () => {
-        if (!this._textAreaRef) {
-            return;
-        }
-
-        this._textAreaRef.style.height = "auto";
-        const isTooBig = this._textAreaRef.scrollHeight > MAX_HEIGHT;
-        if (isTooBig) {
-            this._textAreaRef.style.height = MAX_HEIGHT + "px";
-            this._textAreaRef.style.overflow = "auto";
-        } else {
-            this._textAreaRef.style.height = this._textAreaRef.scrollHeight + "px";
-        }
-    };
-
-    private _isElementOfComponent = (element: any) => {
-        return (
-            [
-                this._textareaContainer,
-                this._textAreaRef,
-                this._sendButtonRef,
-                this._actionsContainer
-            ].indexOf(element) !== -1
-        );
-    };
-
-    private _handleOnSend = () => {
-        if (this.state.text === "") {
-            return;
-        }
-
-        this.props.onSubmit(this.state.text);
-        this._resetTextAreaAfterSend();
-    };
-
-    private _resetTextAreaAfterSend() {
-        this.setState({ text: "" });
-
-        if (!this._textAreaRef) {
-            return;
-        }
-
-        this._textAreaRef.style.height = "auto";
-        this._textAreaRef.focus();
+    if (!this._textareaContainer) {
+      return;
     }
 
-    private _handleNewLineOrSubmit = (e: any) => {
-        let key = e.key || e.which || e.keyCode;
-        if ((key === "Enter" || key == 13) && !e.shiftKey) {
-            this._handleOnSend();
-            e.preventDefault();
-            e.stopPropagation();
-            return false;
-        }
-        return true;
-    };
+    this._textareaContainer.addEventListener("focusin", this._handleFocusIn);
+    this._textareaContainer.addEventListener("focusout", this._handleFocusOut);
 
-    render() {
-        const { text, open, bleepingAnimation } = this.state;
-        const {
-            enableBlackInputTheme,
-            placeholder,
-            disabled,
-            showLockIcon,
-            enableAnimation
-        } = this.props;
+    if (this.props.initialFocus) {
+      this.focus();
+    }
+  }
 
-        return (
-            <div
-                className={styles.textareaContainer}
-                ref={textareaContainer => (this._textareaContainer = textareaContainer)}
-            >
-                {showLockIcon && (
-                    <i
-                        className={classNames(styles.ignoreClicks, {
-                            [styles.privateIcon]: open,
-                            [styles.beatingPrivateIcon]: bleepingAnimation
-                        })}
-                    />
-                )}
-                <textarea
-                    value={text}
-                    className={classNames(styles.textarea, {
-                        [styles.blackInputTheme]: enableBlackInputTheme
-                    })}
-                    ref={textArea => (this._textAreaRef = textArea)}
-                    onInput={this._handleOnInputChange}
-                    onKeyDown={this._handleNewLineOrSubmit}
-                    placeholder={placeholder}
-                    rows={1}
-                    maxLength={MAX_NUM_OF_CHARS}
-                />
-                <div
-                    className={classNames({
-                        [styles.inputActionsContainer]: open,
-                        [styles.inputActionsContainerAnimation]: open && enableAnimation,
-                        [styles.hide]: !open,
-                        [styles.hideAnimation]: !open && enableAnimation
-                    })}
-                    ref={element => (this._actionsContainer = element)}
-                >
-                    <span>{`${text.length}/${MAX_NUM_OF_CHARS}`}</span>
-                    <button
-                        onClick={this._handleOnSend}
-                        className={styles.sendButton}
-                        type={"button"}
-                        disabled={!text.length || disabled}
-                        ref={button => (this._sendButtonRef = button)}
-                    >
-                        {"Send"}
-                    </button>
-                </div>
-            </div>
-        );
+  private _handleFocusIn = () => {
+    if (this._allowClickTimeout) {
+      clearTimeout(this._allowClickTimeout);
+      this._allowClickTimeout = null;
     }
 
-    componentWillUnmount(): void {
-        this._textareaContainer.removeEventListener("focusin", this._handleFocusIn);
-        this._textareaContainer.removeEventListener("focusout", this._handleFocusOut);
+    this.setState({ open: true });
 
-        if (this._allowClickTimeout) {
-            clearTimeout(this._allowClickTimeout);
-            this._allowClickTimeout = null;
-        }
+    if (this.props.onFocusIn) {
+      this.props.onFocusIn();
     }
+  };
+
+  private _handleFocusOut = (e: any) => {
+    if (this._isElementOfComponent(e.relatedTarget)) {
+      return;
+    }
+
+    if (!this.props.alwaysOpen) {
+      // this helps to catch the click on an outside element (like, button) when clicking outsides the element.
+      // otherwise the click is missed and swallowed.
+      this._allowClickTimeout = setTimeout(() => {
+        this.setState(() => ({ open: false }));
+      }, 200);
+    }
+
+    if (this.props.onFocusOut) {
+      this.props.onFocusOut();
+    }
+  };
+
+  focus = () => {
+    if (this._textAreaRef) {
+      this._textAreaRef.focus();
+    }
+  };
+
+  private _handleOnInputChange = (event: any) => {
+    this.setState({ text: event.target.value });
+    this._resize();
+  };
+
+  private _resize = () => {
+    if (!this._textAreaRef) {
+      return;
+    }
+
+    this._textAreaRef.style.height = "auto";
+    const isTooBig = this._textAreaRef.scrollHeight > MAX_HEIGHT;
+    if (isTooBig) {
+      this._textAreaRef.style.height = MAX_HEIGHT + "px";
+      this._textAreaRef.style.overflow = "auto";
+    } else {
+      this._textAreaRef.style.height = this._textAreaRef.scrollHeight + "px";
+    }
+  };
+
+  private _isElementOfComponent = (element: any) => {
+    return (
+      [
+        this._textareaContainer,
+        this._textAreaRef,
+        this._sendButtonRef,
+        this._actionsContainer
+      ].indexOf(element) !== -1
+    );
+  };
+
+  private _handleOnSend = () => {
+    if (this.state.text === "") {
+      return;
+    }
+
+    this.props.onSubmit(this.state.text);
+    this._resetTextAreaAfterSend();
+  };
+
+  private _resetTextAreaAfterSend() {
+    this.setState({ text: "" });
+
+    if (!this._textAreaRef) {
+      return;
+    }
+
+    this._textAreaRef.style.height = "auto";
+    this._textAreaRef.focus();
+  }
+
+  private _handleNewLineOrSubmit = (e: any) => {
+    let key = e.key || e.which || e.keyCode;
+    if ((key === "Enter" || key == 13) && !e.shiftKey) {
+      this._handleOnSend();
+      e.preventDefault();
+      e.stopPropagation();
+      return false;
+    }
+    return true;
+  };
+
+  render() {
+    const { text, open } = this.state;
+    const {
+      enableBlackInputTheme,
+      placeholder,
+      disabled,
+      enableAnimation
+    } = this.props;
+
+    return (
+      <div
+        className={styles.textareaContainer}
+        ref={textareaContainer => (this._textareaContainer = textareaContainer)}
+      >
+        <textarea
+          value={text}
+          className={classNames(styles.textarea, {
+            [styles.blackInputTheme]: enableBlackInputTheme
+          })}
+          ref={textArea => (this._textAreaRef = textArea)}
+          onInput={this._handleOnInputChange}
+          onKeyDown={this._handleNewLineOrSubmit}
+          placeholder={placeholder}
+          rows={1}
+          maxLength={MAX_NUM_OF_CHARS}
+        />
+        <div
+          className={classNames({
+            [styles.inputActionsContainer]: open,
+            [styles.inputActionsContainerAnimation]: open && enableAnimation,
+            [styles.hide]: !open,
+            [styles.hideAnimation]: !open && enableAnimation
+          })}
+          ref={element => (this._actionsContainer = element)}
+        >
+          <span>{`${text.length}/${MAX_NUM_OF_CHARS}`}</span>
+          <button
+            onClick={this._handleOnSend}
+            className={styles.sendButton}
+            type={"button"}
+            disabled={!text.length || disabled}
+            ref={button => (this._sendButtonRef = button)}
+          >
+            {"Send"}
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  componentWillUnmount(): void {
+    this._textareaContainer.removeEventListener("focusin", this._handleFocusIn);
+    this._textareaContainer.removeEventListener("focusout", this._handleFocusOut);
+
+    if (this._allowClickTimeout) {
+      clearTimeout(this._allowClickTimeout);
+      this._allowClickTimeout = null;
+    }
+  }
 }
