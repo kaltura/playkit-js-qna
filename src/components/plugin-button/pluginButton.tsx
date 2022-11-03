931b4dd45e1fac8a3c7a3076fbfca66ce6ebf7af
Serhii Marchenko <Serhii.marchenko@kaltura.com>
2022-11-03 18:03:49 +0200

    feat(FEV-1555): update ui-managers (add more plugin support)

--- src/components/plugin-button/pluginButton.tsx
+++ src/components/plugin-button/pluginButton.tsx
@@ -1,13 +1,15 @@
-export interface MenuIconProps {
-  showIndication: boolean;
-}
-
 import {h} from 'preact';
 import * as styles from './pluginButton.scss';
+import {ui} from 'kaltura-player-js';
 import {A11yWrapper, OnClick} from '@playkit-js/common';
+import {icons} from '../icons';
 
 const {Tooltip, Icon} = KalturaPlayer.ui.components;
 const {withText, Text} = KalturaPlayer.ui.preacti18n;
+
+export interface MenuIconProps {
+  showIndication: boolean;
+}
 
 const translates = ({isActive}: QnaPluginButtonProps) => {
   return {
@@ -26,26 +28,14 @@ export const QnaPluginButton = withText(translates)(({isActive, onClick, showInd
   return (
     <Tooltip label={otherProps.label} type="bottom">
       <A11yWrapper onClick={onClick}>
-        <button aria-label={otherProps.label} className={[styles.qnaPluginButton, isActive ? styles.active : ''].join(' ')}>
-          <svg
-            width="32px"
-            height="32px"
-            viewBox="0 0 32 32"
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlnsXlink="http://www.w3.org/1999/xlink">
-            <g id="Icons/32/message" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-              <g id="Group" transform="translate(3.000000, 5.000000)">
-                <path
-                  d="M7,21.5091539 L11.5753656,17 L22,17 C23.6568542,17 25,15.6568542 25,14 L25,4 C25,2.34314575 23.6568542,1 22,1 L4,1 C2.34314575,1 1,2.34314575 1,4 L1,14 C1,15.6568542 2.34314575,17 4,17 L7,17 L7,21.5091539 Z"
-                  id="Combined-Shape"
-                  stroke="#FFFFFF"
-                  stroke-width="2"></path>
-                <rect id="Rectangle-Copy" fill="#FFFFFF" x="6" y="6" width="15" height="2" rx="1"></rect>
-                <rect id="Rectangle-Copy-2" fill="#FFFFFF" x="6" y="10" width="11" height="2" rx="1"></rect>
-              </g>
-            </g>
-          </svg>
+        <button aria-label={otherProps.label} className={[ui.style.upperBarIcon, styles.qnaPluginButton, isActive ? styles.active : ''].join(' ')}>
+          <Icon
+            id="navigation-plugin-button"
+            height={icons.BigSize}
+            width={icons.BigSize}
+            viewBox={`0 0 ${icons.BigSize} ${icons.BigSize}`}
+            path={icons.PLUGIN_ICON}
+          />
           {showIndication && <span className={styles.indicator} />}
         </button>
       </A11yWrapper>
